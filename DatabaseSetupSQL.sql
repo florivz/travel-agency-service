55a3425096ce8dd1e9ca398c2eb8ff7645b99c0b
cedrik-baedorf <118275915+cedrik-baedorf@users.noreply.github.com>
2023-03-22 18:28:35 +0100

    Add files via upload
    SQL File to create database, db tables and mock data for tables personal_data and address

---
+++ DatabaseSetupSQL.sql
@@ -0,0 +1,67 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server Version:               10.10.2-MariaDB - mariadb.org binary distribution
+-- Server Betriebssystem:        Win64
+-- HeidiSQL Version:             12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Exportiere Datenbank Struktur für travel-agency-service_db
+CREATE DATABASE IF NOT EXISTS `travel-agency-service_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `travel-agency-service_db`;
+
+-- Exportiere Struktur von Tabelle travel-agency-service_db.address
+CREATE TABLE IF NOT EXISTS `address` (
+  `address_id` INT(11) NOT NULL COMMENT 'address''s unique identification number',
+  `street` VARCHAR(60) NOT NULL COMMENT 'street name',
+  `number` VARCHAR(10) NOT NULL COMMENT 'house number',
+  `zip` VARCHAR(10) COMMENT 'zip code if existent',
+  `town` VARCHAR(50) NOT NULL COMMENT 'town name',
+  `country` VARCHAR(50) NOT NULL COMMENT 'country',
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='registered addresses';
+
+-- Exportiere Daten aus Tabelle travel-agency-service_db.address: 4 rows
+DELETE FROM `address`;
+INSERT INTO `address` (`address_id`, `street`, `number`, `zip`, `town`, `country`) VALUES
+	(1, 'Aachener Straße', '999', '50933', 'Köln', 'Deutschland'),
+	(2, 'Hasso-Plattner-Ring', '7', '69190', 'Walldorf', 'Deutschland'),
+	(3, 'Pennsylvania Avenue NW', '1600', '20500', 'Washington, DC', 'United States'),
+	(4, 'Dongzhimen Outer St', '17', '100027', 'Beijing', 'China');
+
+-- Exportiere Struktur von Tabelle travel-agency-service_db.personal_data
+CREATE TABLE IF NOT EXISTS `personal_data` (
+  `personal_data_id` INT(11) NOT NULL COMMENT 'personal_data''s unique identification number',
+  `last_name` VARCHAR(40) NOT NULL COMMENT 'last name',
+  `first_name` VARCHAR(40) NOT NULL COMMENT 'first name',
+  `middle_names` VARCHAR(200) COMMENT 'middle names',
+  `date_of_birth` DATE NOT NULL COMMENT 'date of birth',
+  `address_id` INT(11) NOT NULL COMMENT 'country',
+  PRIMARY KEY (`personal_data_id`),
+  CONSTRAINT `fk_address` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='registered personal data';
+
+-- Exportiere Daten aus Tabelle travel-agency-service_db.personal_data: 6 rows
+DELETE FROM `personal_data`;
+INSERT INTO `personal_data` (`personal_data_id`, `last_name`, `first_name`, `middle_names`, `date_of_birth`, `address_id`) VALUES
+	(1, 'Köln', '1. FC', '', '1948-02-13', 1),
+	(2, 'Hopp', 'Dietmar', '', '1940-04-26', 2),
+	(3, 'Plattner', 'Hasso', '', '1944-01-21', 2),
+	(4, 'Biden', 'Joseph', 'Robinette', '1942-11-20', 3),
+	(5, 'Made-Up', 'Carina', 'Elia Melissa', '1988-04-18', 4),
+	(6, 'Extrovagant', 'Markus-Elias', 'Hoyang Phillippe', '1991-12-31', 4);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
