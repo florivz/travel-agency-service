0c7aac80f7a7b526ff6cee744f6f32898cad7e03
florivz <73907667+florivz@users.noreply.github.com>
2023-03-22 15:23:34 +0100

    Add files via upload

---
+++ TravelAgency/src/main/java/travel/travelagency/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package travel.travelagency.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    private String text = "Hello";
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText(text);
+    }
+}
