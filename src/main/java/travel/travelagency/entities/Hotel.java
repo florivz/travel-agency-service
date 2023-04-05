60b7cf025a46af5e7ee5e516e94ff61c8d0ee048
I551381 <cedrik.baedorf@sap.com>
2023-04-05 08:11:27 +0200

    javadoc for Hotel, FlightConnection

--- src/main/java/travel/travelagency/entities/Hotel.java
+++ src/main/java/travel/travelagency/entities/Hotel.java
@@ -9,32 +9,64 @@ import javax.persistence.Column;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 
+/**
+ * This class is a jpa entity to the corresponding table 'HOTEL'
+ * in the database 'travel-agency-service_db'.
+ * @author I551381
+ * @version 1.0
+ */
 @Entity
 @Table(name = "HOTEL")
 public class Hotel {
 
+  /**
+   * Unique identifier for each hotel record (primary key in the database).
+   * This value will be generated automatically.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "HOTEL_ID")
   private Integer id;
 
+  /**
+   * hotel name
+   */
   @Column(name = "NAME")
   private String name;
 
+  /**
+   * price per night per person
+   */
   @Column(name = "PRICE_PER_PERSON")
   private Double pricePerPerson;
 
+  /**
+   * three character currency key to the <code>pricePerPerson</code>
+   */
   @Column(name = "CURRENCY_KEY")
   private String currencyKey;
 
+  /**
+   * hotel address
+   */
   @ManyToOne
   @JoinColumn(name = "ADDRESS_ID")
   private Address address;
 
+  /**
+   * Constructor creates a <code>Hotel</code> object with initial attributes
+   */
   public Hotel() {
 
   }
 
+  /**
+   * Constructor creates a <code>Hotel</code> object with specified attributes
+   * @param name hotel name
+   * @param pricePerPerson price per night per person
+   * @param currencyKey three character currency key to the <code>pricePerPerson</code>
+   * @param address hotel address
+   */
   public Hotel(int id, String name, double pricePerPerson, String currencyKey, Address address) {
     this.id = id;
     this.name = name;
@@ -43,42 +75,74 @@ public class Hotel {
     this.address = address;
   }
 
+  /**
+   * Getter-method for the <code>id</code> attribute.
+   * @return unique identification number
+   */
   public Integer getId() {
     return id;
   }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
+  /**
+   * Getter-method for the <code>name</code> attribute.
+   * @return hotel name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Setter-method for the <code>name</code> attribute.
+   * @param name new hotel name
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Getter-method for the <code>pricePerPerson</code> attribute.
+   * @return price per night per person
+   */
   public Double getPricePerPerson() {
     return pricePerPerson;
   }
 
-  public void setPricePerPerson(Double price) {
-    this.pricePerPerson = price;
+  /**
+   * Setter-method for the <code>pricePerPerson</code> attribute.
+   * @param pricePerPerson new price per night per person
+   */
+  public void setPricePerPerson(Double pricePerPerson) {
+    this.pricePerPerson = pricePerPerson;
   }
 
+  /**
+   * Getter-method for the <code>currencyKey</code> attribute.
+   * @return three character currency key to the <code>pricePerPerson</code>
+   */
   public String getCurrencyKey() {
     return currencyKey;
   }
 
-  public void setCurrencyKey(String currency) {
-    this.currencyKey = currency;
+  /**
+   * Setter-method for the <code>currencyKey</code> attribute.
+   * @param currencyKey new three character currency key to the <code>pricePerPerson</code>
+   */
+  public void setCurrencyKey(String currencyKey) {
+    this.currencyKey = currencyKey;
   }
 
+  /**
+   * Getter-method for the <code>address</code> attribute.
+   * @return hotel address
+   */
   public Address getAddress() {
     return address;
   }
 
+  /**
+   * Setter-method for the <code>address</code> attribute.
+   * @param address new hotel address
+   */
   public void setAddress(Address address) {
     this.address = address;
   }
@@ -102,10 +166,10 @@ public class Hotel {
   @Override
   public String toString() {
     return
-        (name != null                             ? name.toString() + '\n'                                    : "" )
+        (name != null                             ? name + '\n'                                    : "" )
             + (address != null                    ? address.toString() + '\n'                                 : "" )
             + (pricePerPerson != null && currencyKey
-            != null  ? "Price: " + pricePerPerson.toString() + " " + currencyKey.toString() : "" );
+            != null  ? "Price: " + pricePerPerson + " " + currencyKey : "" );
   }
 
   @Override
