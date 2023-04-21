a17c61fb04fbaac93a7de842a27433b476679d87
I551381 <cedrik.baedorf@sap.com>
2023-04-21 11:17:16 +0200

    Booking javadoc

--- src/main/java/travel/travelagency/entities/Booking.java
+++ src/main/java/travel/travelagency/entities/Booking.java
@@ -14,10 +14,12 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 /**
- * This class is a jpa entity to the corresponding table 'BOOKING' in the database 'travel-agency-service_db'.
+ * This class is a jpa entity to the corresponding table 'BOOKING'
  * It contains several prepared statements that allow for queries of filtered bookings
+ * in the database 'travel-agency-service_db'.
+ * @author I551381
+ * @version 1.0
  */
 @Entity
 @Table(name = "BOOKING")
@@ -38,58 +40,129 @@ import javax.persistence.Table;
 public class Booking {
 
   public static final String
+      /**
+       * This constant represents the name of a named query returning all bookings
+       */
       FIND_ALL = "Booking.findAll",
+
+      /**
+       * This constant represents the name of a named query returning all bookings filtered by
+       * bookingID, customerID, and lastName of the customer
+       */
       FIND_WITH_FILTERS = "Booking.findWithFilters",
       BOOKING_ID = "bookingID",
       CUSTOMER_ID = "customerID",
       CUSTOMER_NAME = "customerName";
 
+  /**
+   * Unique identifier for each booking record (primary key in the database).
+   * This value will be generated automatically.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "BOOKING_ID")
   private Integer id;
 
+  /**
+   * Customer paying for this booking
+   */
   @ManyToOne
   @JoinColumn(name = "CUSTOMER_ID")
   private Customer customer;
 
+  /**
+   * Date of this booking
+   */
   @Column(name = "DATE")
   private LocalDate date;
 
+  /**
+   * Set of all trips associated with this booking
+   */
   @OneToMany
   @JoinColumn(name = "BOOKING_ID")
   private Set<Trip> tripSet;
 
+  /**
+   * Constructor creates a <code>Booking</code> object with initial attributes
+   */
   public Booking() {
 
   }
 
+  /**
+   * Constructor creates a <code>Booking</code> object with specified attributes
+   * @param customer customer paying for this booking
+   * @param date date of this booking
+   * @param tripSet set of all trips associated with this booking
+   */
   public Booking(Customer customer, LocalDate date, Set<Trip> tripSet) {
     this.date = date;
     this.customer = customer;
     this.tripSet = tripSet;
   }
 
+  /**
+   * Getter-method for the <code>id</code> attribute.
+   * @return unique identification number
+   */
   public Integer getId() {
     return id;
   }
 
+  /**
+   * Getter-method for the <code>customer</code> attribute.
+   * @return customer paying for this booking
+   */
   public Customer getCustomer() {
     return customer;
   }
 
+  /**
+   * Setter-method for the <code>customer</code> attribute.
+   * @param customer new customer paying for this booking
+   */
   public void setCustomer(Customer customer) {
     this.customer = customer;
   }
 
+  /**
+   * Getter-method for the <code>date</code> attribute.
+   * @return date of this booking
+   */
+  public LocalDate getDate() {
+    return date;
+  }
+
+  /**
+   * Setter-method for the <code>date</code> attribute.
+   * @param date new date of this booking
+   */
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  /**
+   * Getter-method for the <code>tripSet</code> attribute.
+   * @return set of all trips associated with this booking
+   */
   public Set<Trip> getTripSet() {
     return tripSet;
   }
 
-  public void setTripSet(Set<Trip> tripList) {
-    this.tripSet = tripList;
+  /**
+   * Setter-method for the <code>tripSet</code> attribute.
+   * @param tripSet new set of all trips associated with this booking
+   */
+  public void setTripSet(Set<Trip> tripSet) {
+    this.tripSet = tripSet;
   }
 
+  /**
+   * This method returns the total price of this single booking by
+   * adding the total price of all trips associated with this booking.
+   * @return total price of this booking
+   */
   public double getTotalPrice() {
     return tripSet.stream().mapToDouble(Trip::getTotalPrice).sum();
   }
