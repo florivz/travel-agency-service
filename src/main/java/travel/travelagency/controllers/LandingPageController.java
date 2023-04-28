9df609662c81deb86e5ff7e88514271395a25196
I551381 <cedrik.baedorf@sap.com>
2023-04-28 13:06:30 +0200

    midway commit 3

--- src/main/java/travel/travelagency/controllers/LandingPageController.java
+++ src/main/java/travel/travelagency/controllers/LandingPageController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -21,10 +22,13 @@ public class LandingPageController {
     static final Logger logger = LogManager.getLogger(LandingPageController.class);
 
     public static final String VIEW_NAME = "landing_page.fxml";
+    @FXML public Group credentialsGroup;
     @FXML public Group usernameGroup;
-    @FXML public Group usernameFrame;
+    @FXML private TextField usernameTextField;
     @FXML public Group passwordGroup;
-    @FXML public Group passwordFrame;
+    @FXML private PasswordField passwordTextField;
+    @FXML public Group infoMessageGroup;
+    @FXML public Text infoMessage;
     @FXML public Group loginButtonGroup;
     @FXML public Group loginButtonFrame;
 
@@ -34,9 +38,36 @@ public class LandingPageController {
     @FXML private ImageView ellipse_2;
     @FXML private ImageView rectangle_3;
     @FXML private Text log_in;
-    @FXML private PasswordField passwordTextField;
-    @FXML private TextField usernameTextField;
     @FXML private Text headerMessage;
+
+    private static class LoginThread extends Thread {
+
+        private final TextField USERNAME, PASSWORD;
+        private final Text INFO;
+
+        public LoginThread(TextField username, TextField password, Text info) {
+            this.USERNAME = username;
+            this.PASSWORD = password;
+            this.INFO = info;
+        }
+
+        @Override
+        public void run() {
+            try {
+                TravelAgencyEntityManagerFactory factory = TravelAgencyDatabaseAuthenticator.loginToDataBase(
+                        USERNAME.getText(), PASSWORD.getText()
+                );
+                TravelAgencyServiceApplication.setRoot(StartingPageController.VIEW_NAME);
+            } catch (RuntimeException | IOException e) {
+                logger.info(e.getMessage());
+                USERNAME.clear();
+                PASSWORD.clear();
+                INFO.setFill(Color.RED);
+                INFO.setText("Invalid credentials");
+            }
+        }
+
+    }
 
     public void initialize() throws IOException{
         //custom code here
@@ -49,16 +80,9 @@ public class LandingPageController {
 
     @FXML
     private void _login_onClick(ActionEvent actionEvent) throws IOException {
-        try {
-            TravelAgencyEntityManagerFactory factory = TravelAgencyDatabaseAuthenticator.loginToDataBase(
-                    usernameTextField.getText(), passwordTextField.getText()
-            );
-            TravelAgencyServiceApplication.setRoot(StartingPageController.VIEW_NAME);
-        } catch (RuntimeException e) {
-            logger.info(e.getMessage());
-            usernameTextField.clear();
-            passwordTextField.clear();
-        }
+        infoMessage.setFill(Color.BLACK);
+        infoMessage.setText("Attempting login...");
+        new LoginThread(usernameTextField, passwordTextField, infoMessage).start();
     }
 
 
